99831495f72cd76f15674bd607237f43a5db30c3
Artur Soler <artur@artursoler.com>
2015-05-28 18:27:40 +0200

    TcpExample

---
+++ src/main/scala/TcpStream.scala
@@ -0,0 +1,26 @@
+import akka.actor.ActorSystem
+import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl.Tcp.{ServerBinding, IncomingConnection}
+import akka.stream.scaladsl.{Flow, Tcp, Source}
+import akka.util.ByteString
+import scala.concurrent.Future
+
+object TcpStream extends App {
+  implicit val system = ActorSystem("SimpleStream")
+  implicit val materializer = ActorFlowMaterializer()
+  import system.dispatcher
+
+  val connections: Source[IncomingConnection, Future[ServerBinding]] =
+    Tcp().bind("0.0.0.0", 8888)
+
+  connections runForeach { connection =>
+    println(s"New connection from: ${connection.remoteAddress}")
+
+    val echo = Flow[ByteString]
+      .transform(() => Parser.parseLines("\n", maximumLineBytes = 256))
+      .map(_ + "\n")
+      .map(ByteString(_))
+
+    connection.handleWith(echo)
+  }
+}
